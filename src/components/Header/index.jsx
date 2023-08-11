f5c66c0676cbd84fce55b3ad68a8a78b3a7a4b96
fFernanda-gomes <feeh.gomes0110@gmail.com>
2023-08-11 14:16:03 -0300

    ajuste dos icons list e user, e na responsividade

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,43 +1,45 @@
-import menuHamburger from '../../assets/hamburger.svg'
 import logo from '../../assets/logoHyper.png'
-import userIcon from '../../assets/icon-user.svg'
 
 import { HeaderContainer, NavContainer } from './style'
 import { NavLink } from 'react-router-dom'
 
+import { List, User } from '@phosphor-icons/react'
+
 export function Header() {
   return (
     <HeaderContainer>
-      <NavLink to="/">
-        <img
-          src={menuHamburger}
-          alt=""
-          width={32}
-          height={22}
-          className="menu-burger"
-        />
-      </NavLink>
-      <NavLink to="/">
-        <img src={logo} alt="" width={28} height={32} className="logo img" />
-      </NavLink>
-      <a href="">
-        <img
-          src={userIcon}
-          alt=""
-          width={32}
-          height={32}
-          className="user img"
-        />
-      </a>
+      <div className="mobile">
+        <NavLink to="/">
+          <List color="#fff" fontSize={42} weight="bold" className="list" />
+        </NavLink>
 
-      <NavContainer>
-        <NavLink to="/SignIn">objetivos</NavLink>
-        <NavLink to="">público</NavLink>
-        <NavLink to="">equipamentos</NavLink>
-        <NavLink to="">funcionamento</NavLink>
-        <NavLink to="">download</NavLink>
-        <NavLink to="">bibliografia</NavLink>
-      </NavContainer>
+        <NavLink to="/">
+          <img src={logo} alt="" width={28} height={32} className="logo img" />
+        </NavLink>
+
+        <NavLink to="/SignIn">
+          <User color="#fff" fontSize={38} className="user" />
+        </NavLink>
+      </div>
+
+      <div className="web">
+        <NavLink to="/">
+          <img src={logo} alt="" width={28} height={32} className="logo img" />
+        </NavLink>
+
+        <NavContainer>
+          <NavLink to="/SignIn">objetivos</NavLink>
+          <NavLink to="">público</NavLink>
+          <NavLink to="">equipamentos</NavLink>
+          <NavLink to="">funcionamento</NavLink>
+          <NavLink to="">download</NavLink>
+          <NavLink to="">bibliografia</NavLink>
+        </NavContainer>
+
+        <NavLink to="/SignIn">
+          <User color="#fff" fontSize={38} className="user" />
+        </NavLink>
+      </div>
     </HeaderContainer>
   )
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,60 +2,49 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
   padding: 32px 48px 72px 48px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
   font-family: 'Montserrat', sans-serif;
 
+  .mobile {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .list {
+      display: block;
+    }
+  }
+
+  .web {
+    display: none;
+  }
+
   @media (min-width: 950px) {
-    justify-content: center;
     padding-top: 64px;
     padding-bottom: 112px;
 
-    .menu-burger {
+    .mobile {
       display: none;
     }
 
-    .img {
-      position: absolute;
-    }
+    .web {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
 
-    .logo {
-      left: 80px;
-      width: 32px;
-      height: 44px;
-    }
-
-    .user {
-      top: 55px;
-      right: 90px;
+      .list {
+        display: none;
+      }
     }
   }
 
   @media (min-width: 1440px) {
-    justify-content: center;
-    align-items: center;
     padding-top: 64px;
     padding-bottom: 112px;
 
-    .menu-burger {
-      display: none;
-    }
-
-    .img {
-      position: absolute;
-    }
-
-    .logo {
-      left: 96px;
-      width: 42px;
-      height: 54px;
-    }
-
-    .user {
-      top: 55px;
-      right: 110px;
+    .web {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
     }
   }
 `
@@ -76,8 +65,6 @@ export const NavContainer = styled.nav`
   }
 
   @media (min-width: 1440px) {
-    display: flex;
-    padding-inline: 115px;
     gap: 50px;
 
     a {

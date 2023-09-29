23f4195c3b35b4877a170277007536cc0b5b22d0
fFernanda-gomes <feeh.gomes0110@gmail.com>
2023-09-28 23:11:31 -0300

    carrosséis feitps

--- src/pages/Hardware/index.jsx
+++ src/pages/Hardware/index.jsx
@@ -1,104 +1,275 @@
 import {
+  Align,
   Container,
-  ImageProduct,
+  DOTS,
   Info,
   Main,
   NameProcuct,
+  Navigation,
   Price,
   PriceContainer,
   ProductContainer,
   Title,
+  Visible,
+  VisibleWeb,
 } from './styles'
+
+import Cifrao from '../../assets/hardwares/cifrao.png'
 
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
-
-import Cifrao from '../../assets/hardwares/cifrao.png'
-import Cabo from '../../assets/hardwares/caboAdaptador.png'
-import Arduino from '../../assets/hardwares/arduino.png'
-import Bateria from '../../assets/hardwares/bateria9V.png'
-import Buzzer from '../../assets/hardwares/Buzzer.png'
-import bluetooth from '../../assets/hardwares/PlacaBluetooth.png'
-import protoboard from '../../assets/hardwares/Protoboard.png'
-import jumper from '../../assets/hardwares/JumpersMF.png'
 import { useState } from 'react'
 
 export function Hardware() {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
+    created() {
+      setLoaded(true)
+    },
+  })
+
+  const [sliderRefWeb] = useKeenSlider()
+
   return (
-    <Main>
-      <Title>
-        <span>Equipamentos</span> e Valores
-      </Title>
+    <Align>
+      <Main>
+        <Title>
+          <span>Equipamentos</span> e Valores
+        </Title>
 
-      <Container>
-        <ProductContainer className="keen-slider__slide number-slide1">
-          {/* <ImageProduct src={Cabo} alt="" /> */}
-          <Info>
-            <NameProcuct>
-              CABO ADAPTADOR <br /> BATERIA 9V
-            </NameProcuct>
-            <PriceContainer>
-              <img src={Cifrao} width={9} height={9} alt="" />
-              <Price>R$ 00</Price>
-            </PriceContainer>
-          </Info>
-        </ProductContainer>
+        <Visible>
+          <Navigation ref={sliderRef} className="keen-slider">
+            <Container className="keen-slider__slide number-slide1">
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
 
-        {/* <ProductContainer className="keen-slider__slide number-slide1">
-          <ImageProduct src={jumper} alt="" />
-          <Info>
-            <NameProcuct>JUMPERS</NameProcuct>
-            <PriceContainer>
-              <img src={Cifrao} width={9} height={9} alt="" />
-              <Price>R$ 00</Price>
-            </PriceContainer>
-          </Info>
-        </ProductContainer>
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
 
-        <ProductContainer className="keen-slider__slide number-slide1">
-          <ImageProduct src={bluetooth} alt="" />
-          <Info>
-            <NameProcuct>MÓDULO HC-05</NameProcuct>
-            <PriceContainer>
-              <img src={Cifrao} width={9} height={9} alt="" />
-              <Price>R$ 00</Price>
-            </PriceContainer>
-          </Info>
-        </ProductContainer>
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
 
-        <ProductContainer className="keen-slider__slide number-slide1">
-          <ImageProduct src={protoboard} alt="" />
-          <Info>
-            <NameProcuct>PROTOBOARD</NameProcuct>
-            <PriceContainer>
-              <img src={Cifrao} width={9} height={9} alt="" />
-              <Price>R$ 00</Price>
-            </PriceContainer>
-          </Info>
-        </ProductContainer>
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+            </Container>
+            <Container className="keen-slider__slide number-slide2">
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
 
-        <ProductContainer className="keen-slider__slide number-slide2">
-          <ImageProduct src={protoboard} alt="" />
-          <Info>
-            <NameProcuct>PROTOBOARD</NameProcuct>
-            <PriceContainer>
-              <img src={Cifrao} width={9} height={9} alt="" />
-              <Price>R$ 00</Price>
-            </PriceContainer>
-          </Info>
-        </ProductContainer>
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
 
-        <ProductContainer className="keen-slider__slide number-slide2">
-          <ImageProduct src={protoboard} alt="" />
-          <Info>
-            <NameProcuct>PROTOBOARD</NameProcuct>
-            <PriceContainer>
-              <img src={Cifrao} width={9} height={9} alt="" />
-              <Price>R$ 00</Price>
-            </PriceContainer>
-          </Info>
-        </ProductContainer> */}
-      </Container>
-    </Main>
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+            </Container>
+          </Navigation>
+        </Visible>
+
+        <DOTS>
+          {loaded && instanceRef.current && (
+            <div className="dots">
+              {[
+                ...Array(
+                  instanceRef.current.track.details.slides.length,
+                ).keys(),
+              ].map((idx) => {
+                return (
+                  <button
+                    key={idx}
+                    onClick={() => {
+                      instanceRef.current?.moveToIdx(idx)
+                    }}
+                    className={'dot' + (currentSlide === idx ? ' active' : '')}
+                  ></button>
+                )
+              })}
+            </div>
+          )}
+        </DOTS>
+
+        <VisibleWeb>
+          <Navigation ref={sliderRefWeb} className="keen-slider">
+            <Container className="keen-slider__slide number-slide1">
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+            </Container>
+            <Container className="keen-slider__slide number-slide2">
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+
+              <ProductContainer>
+                <Info>
+                  <NameProcuct>
+                    CABO ADAPTADOR <br /> BATERIA 9V
+                  </NameProcuct>
+                  <PriceContainer>
+                    <img src={Cifrao} width={9} height={9} alt="" />
+                    <Price>R$ 00</Price>
+                  </PriceContainer>
+                </Info>
+              </ProductContainer>
+            </Container>
+          </Navigation>
+        </VisibleWeb>
+      </Main>
+    </Align>
   )
 }
--- src/pages/Hardware/styles.js
+++ src/pages/Hardware/styles.js
@@ -1,8 +1,52 @@
 import styled from 'styled-components'
 import Arduino from '../../assets/hardwares/arduino.svg'
+import Jumpers from '../../assets/hardwares/jumpers.svg'
+
+export const DOTS = styled.div`
+  @media (min-width: 850px) {
+    display: none;
+  }
+`
+
+export const Align = styled.div`
+  height: 100vh;
+`
+
 
 export const Main = styled.main`
   padding: 0 3rem 5.75rem 3rem;
+
+  .dots {
+    display: flex;
+    padding: 10px 0;
+    justify-content: center;
+  }
+
+  .dot {
+    border: none;
+    width: 10px;
+    height: 10px;
+    background: #c5c5c5;
+    border-radius: 50%;
+    margin: 0 5px;
+    padding: 5px;
+    cursor: pointer;
+  }
+
+  .dot:focus {
+    outline: none;
+  }
+
+  .dot.active {
+    background: #000;
+  }
+
+  @media (min-width: 850px) {
+    margin: 0 6rem 4rem 6rem;
+    border-radius: 2rem;
+    background: rgba(0, 0, 0, 0.8);
+    padding: 4.06rem 4rem 6.56rem 4rem;
+  }
 `
 export const Title = styled.h1`
   font-family: 'Montserrat', sans-serif;
@@ -14,36 +58,66 @@ export const Title = styled.h1`
   span {
     color: ${({ theme }) => theme.colors.primary_medium};
   }
+
+  @media (min-width: 850px) {
+    font-size: 2.5rem;
+  }
 `
+export const Visible = styled.div`
+  @media (min-width: 850px) {
+    display: none;
+  }
+`
+
+export const VisibleWeb = styled.div`
+  display: none;
+
+  @media (min-width: 850px) {
+    display: flex;
+  }
+`
+
+export const Navigation = styled.div``
 
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  gap: 2rem;
+  gap: 1rem;
+
+  @media (min-width: 850px) {
+    margin-left: 1rem;
+    display: flex;
+    gap: 3rem;
+  }
 `
 
 export const ProductContainer = styled.div`
-  /* margin-bottom: 3rem; */
-  /* width: 125px; */
   background: url(${Arduino});
   width: 7.875rem;
   height: 12rem;
+
+  @media (min-width: 850px) {
+    background: url(${Jumpers});
+    width: 15.75rem;
+    height: 24rem;
+  }
 `
 
-export const ImageProduct = styled.img`
-  /* width: 125px;
-  height: 192px; */
-`
 export const Info = styled.div`
-  /* text-align: center;
-  margin-top: -3rem;
-  position: absolute;
-  z-index: 3;
-  margin-left: 1.5rem; */
+  height: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  padding: 1rem;
+
+  @media (min-width: 850px) {
+    padding-bottom: 1.4rem;
+  }
 `
 
 export const NameProcuct = styled.h3`
-  /* color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.white};
   font-family: 'Montserrat', sans-serif;
   font-size: 0.5rem;
   font-weight: 700;
@@ -51,17 +125,33 @@ export const NameProcuct = styled.h3`
   border-radius: 1rem;
   border: 1px solid #36a7db;
   margin-bottom: 0.4rem;
-  padding: 0.2rem; */
+  padding: 0.2rem;
+
+  @media (min-width: 850px) {
+    font-size: 1rem;
+    padding: 0.4rem;
+  }
 `
 export const PriceContainer = styled.div`
-  /* justify-content: center;
+  justify-content: center;
   align-items: center;
   display: flex;
-  gap: 0.22rem; */
+  gap: 0.3rem;
+
+  @media (min-width: 850px) {
+    img {
+      width: 1.125rem;
+      height: 1.125rem;
+    }
+  }
 `
 
 export const Price = styled.strong`
-  /* color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.white};
   font-family: 'Montserrat', sans-serif;
-  font-size: 0.6rem; */
+  font-size: 0.6rem;
+
+  @media (min-width: 850px) {
+    font-size: 0.8rem;
+  }
 `
